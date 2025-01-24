05e50f993cfe3b6164b9474c91ba42f3b335886f
lpelkey23 <lpelkey23@yahoo.com>
2025-01-24 21:36:27 +0000

    Message is sent from Vision to Display

--- display/src/pipe.cpp
+++ display/src/pipe.cpp
@@ -1,7 +1,34 @@
 #include <glog/logging.h>
 #include <iostream>
 
-void displayEntry() {
+#include "../../pipes.h"
+#include "pipe.h"
+
+void displayEntry(struct DisplayPipes pipes) {
   LOG(INFO) << "Within display process";
-  std::cout << "DISPLAY" << std::endl;
+
+  // Close unused ends of the pipes
+  close(getPipe(pipes.toVision, READ));    // Display does not read from toVision
+  close(getPipe(pipes.toHardware, READ)); // Display does not read from toHardware
+  close(getPipe(pipes.fromVision, WRITE));  // Display does not write to fromVision
+  close(getPipe(pipes.fromHardware, WRITE)); // Display does not write to fromHardware
+
+  // Close not currently used ends
+  close(getPipe(pipes.fromHardware, READ)); // Not currently used
+  close(getPipe(pipes.toVision, WRITE));    // Not currently used
+  close(getPipe(pipes.toHardware, WRITE));  // Not currently used
+
+  LOG(INFO) << "Receiving message from Vision process";
+  char buffer[256];
+  ssize_t bytesRead = read(getPipe(pipes.fromVision, READ), buffer, sizeof(buffer) - 1);
+  if (bytesRead > 0) {
+    buffer[bytesRead] = '\0'; // Null-terminate the received string
+    LOG(INFO) << "Display received message: " << buffer;
+    printf(buffer);
+  } else {
+    LOG(FATAL) << "Failed to read from Vision process";
+  }
+
+  // Close the read end after processing
+  close(getPipe(pipes.fromVision, READ));
 }
--- vision/src/pipe.cpp
+++ vision/src/pipe.cpp
@@ -1,7 +1,27 @@
 #include <glog/logging.h>
 #include <iostream>
 
-void visionEntry() {
+#include "pipe.h"
+
+// 0 reading
+// 1 writing
+
+void visionEntry(struct VisionPipes pipes) {
   LOG(INFO) << "Within vision process";
-  std::cout << "VISION" << std::endl;
+  //not ever used
+  close(getPipe(pipes.fromDisplay, WRITE));
+  close(getPipe(pipes.fromHardware, WRITE));
+  close(getPipe(pipes.toDisplay, READ));
+  close(getPipe(pipes.toHardware, READ));
+
+  //not currently used
+  close(getPipe(pipes.fromHardware, READ));
+  close(getPipe(pipes.fromDisplay, READ));
+  close(getPipe(pipes.toHardware, WRITE));
+
+  LOG(INFO) << "Sending Message from Vision to Display";
+  const char* message = "Hello from Vision!";
+  write(getPipe(pipes.toDisplay, WRITE), message, strlen(message) + 1);
+  //used
+  close(getPipe(pipes.toDisplay, WRITE));
 }
