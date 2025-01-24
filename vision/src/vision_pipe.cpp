d3ebfdc6b35c9e87c8f06e886decc2da78ef4cbc
Joedahms <dahms.joejoe@gmail.com>
2025-01-24 18:53:06 -0500

    getPipe purge

--- vision/src/vision_pipe.cpp
+++ vision/src/vision_pipe.cpp
@@ -8,20 +8,22 @@
 
 void visionEntry(struct VisionPipes pipes) {
   LOG(INFO) << "Within vision process";
+
   // not ever used
-  close(getPipe(pipes.fromDisplay, WRITE));
-  close(getPipe(pipes.fromHardware, WRITE));
-  close(getPipe(pipes.toDisplay, READ));
-  close(getPipe(pipes.toHardware, READ));
+  close(pipes.fromDisplay[WRITE]);
+  close(pipes.fromHardware[WRITE]);
+  close(pipes.toDisplay[READ]);
+  close(pipes.toHardware[READ]);
 
   // not currently used
-  close(getPipe(pipes.fromHardware, READ));
-  close(getPipe(pipes.fromDisplay, READ));
-  close(getPipe(pipes.toHardware, WRITE));
+  close(pipes.fromHardware[READ]);
+  close(pipes.fromDisplay[READ]);
+  close(pipes.toHardware[WRITE]);
+
+  // used
+  close(pipes.toDisplay[WRITE]);
 
   LOG(INFO) << "Sending Message from Vision to Display";
   const char* message = "Hello from Vision!";
-  write(getPipe(pipes.toDisplay, WRITE), message, strlen(message) + 1);
-  // used
-  close(getPipe(pipes.toDisplay, WRITE));
+  write(pipes.toDisplay[WRITE], message, strlen(message) + 1);
 }
