60b3cc2a01f5a8f37324d3d4ca604f232c97a1d3
lpelkey23 <lpelkey23@yahoo.com>
2025-01-25 02:12:39 +0000

    reduced display_pipe to barebones process

--- display/src/display_pipe.cpp
+++ display/src/display_pipe.cpp
@@ -22,36 +22,8 @@ void displayEntry(struct DisplayPipes pipes) {
   close(pipes.fromHardware[WRITE]); // Display does not write to fromHardware
 
   // Close not currently used ends
-  close(pipes.toVision[WRITE]);   // Not currently used
-  close(pipes.toHardware[WRITE]); // Not currently used
-
-  LOG(INFO) << "Receiving message from Vision process";
-  std::string buffer;
-  char character;
-  while (read(pipes.fromVision[READ], &character, 1) > 0) {
-    if (character != 0) {
-      buffer.push_back(character);
-    }
-  }
-
-  if (buffer.length() == 0) {
-    LOG(FATAL) << "Failed to read from Vision process";
-  }
-  LOG(INFO) << "Display received message: " << buffer;
-  std::cout << buffer << std::endl;
-
-  LOG(INFO) << "Receiving message from Hardware process";
-  std::string buffer1;
-  char character1;
-  while (read(pipes.fromHardware[READ], &character1, 1) > 0) {
-    if (character1 != 0) {
-      buffer1.push_back(character1);
-    }
-  }
-
-  if (buffer1.length() == 0) {
-    LOG(FATAL) << "Failed to read from Hardware process";
-  }
-  LOG(INFO) << "Display received message: " << buffer1;
-  std::cout << buffer1 << std::endl;
+  close(pipes.toVision[WRITE]);    // Not currently used
+  close(pipes.toHardware[WRITE]);  // Not currently used
+  close(pipes.fromHardware[READ]); // Not currently used
+  close(pipes.fromVision[READ]);   // Not currently used
 }
