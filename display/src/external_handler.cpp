81b719a8208db140f006c66cce4eeaebe970272c
Joedahms <dahms.joejoe@gmail.com>
2025-01-31 14:04:16 -0500

    moved open database into its own function

--- display/src/external_handler.cpp
+++ display/src/external_handler.cpp
@@ -16,10 +16,40 @@ void externalHandler(struct DisplayPipes pipes) {
     received = receiveFoodItem(foodItem, pipes.fromVision[READ], timeout);
   }
 
-  sqlite3* database;
-  int sqlReturn = sqlite3_open("test.db", &database);
+  sqlite3* database = nullptr;
+  openDatabase(&database);
+
+  const char* insertSql = "INSERT INTO foodItems (name, catagory) VALUES (?, ?);";
+
+  sqlite3_stmt* statement;
+  int sqlReturn = sqlite3_prepare_v2(database, insertSql, -1, &statement, nullptr);
   if (sqlReturn != SQLITE_OK) {
-    LOG(FATAL) << "Error opening database: " << sqlite3_errmsg(database);
+    LOG(FATAL) << "Prepare error: " << sqlite3_errmsg(database);
+  }
+
+  sqlite3_bind_text(statement, 1, foodItem.name.c_str(), -1, SQLITE_TRANSIENT);
+  sqlite3_bind_text(statement, 2, foodItem.catagory.c_str(), -1, SQLITE_TRANSIENT);
+
+  sqlReturn = sqlite3_step(statement);
+  if (sqlReturn != SQLITE_DONE) {
+    LOG(FATAL) << "Execution Error: " << sqlite3_errmsg(database);
+  }
+  sqlite3_finalize(statement);
+
+  sqlite3_close(database);
+  std::cout << "here" << std::endl;
+}
+
+/**
+ * Open the database and make sure the table for the food items is present.
+ *
+ * @param database Pointer to database pointer so that it can be modified (opened)
+ * @return None
+ */
+void openDatabase(sqlite3** database) {
+  int sqlReturn = sqlite3_open("test.db", database);
+  if (sqlReturn != SQLITE_OK) {
+    LOG(FATAL) << "Error opening database: " << sqlite3_errmsg(*database);
   }
 
   const char* createSqlTable = "CREATE TABLE IF NOT EXISTS foodItems("
@@ -34,38 +64,11 @@ void externalHandler(struct DisplayPipes pipes) {
                                "expirationDateDay INTEGER,"
                                "weight FLOAT,"
                                "quantity INTEGER);";
-  char* errorMessage         = nullptr;
-  sqlReturn = sqlite3_exec(database, createSqlTable, NULL, NULL, &errorMessage);
+
+  char* errorMessage = nullptr;
+  sqlReturn          = sqlite3_exec(*database, createSqlTable, NULL, NULL, &errorMessage);
   if (sqlReturn != SQLITE_OK) {
-    LOG(FATAL) << "SQL Error: " << errorMessage;
+    LOG(FATAL) << "SQL Exec Error: " << errorMessage;
     sqlite3_free(errorMessage);
   }
-
-  const char* insertSql = "INSERT INTO foodItems (name, catagory) VALUES (?, ?);";
-
-  sqlite3_stmt* statement;
-  sqlReturn = sqlite3_prepare_v2(database, insertSql, -1, &statement, nullptr);
-  if (sqlReturn != SQLITE_OK) {
-    LOG(FATAL) << "Prepare error: " << sqlite3_errmsg(database);
-  }
-
-  sqlite3_bind_text(statement, 1, foodItem.name.c_str(), -1, SQLITE_TRANSIENT);
-  sqlite3_bind_text(statement, 2, foodItem.catagory.c_str(), -1, SQLITE_TRANSIENT);
-
-  sqlReturn = sqlite3_step(statement);
-  if (sqlReturn != SQLITE_DONE) {
-    LOG(FATAL) << "Execution Error: " << sqlite3_errmsg(database);
-  }
-  sqlite3_finalize(statement);
-  /*
-  int rc = sqlite3_exec(database, insert_sql, 0, 0, &errorMessage);
-  if (rc != SQLITE_OK) {
-    LOG(FATAL) << "Failed to insert: " << errorMessage;
-    sqlite3_free(errorMessage);
-    sqlite3_close(database);
-  }
-  */
-
-  sqlite3_close(database);
-  std::cout << "here" << std::endl;
 }
--- display/src/external_handler.h
+++ display/src/external_handler.h
@@ -1,8 +1,11 @@
 #ifndef EXTERNAL_HANDLER_H
 #define EXTERNAL_HANDLER_H
 
+#include <sqlite3.h>
+
 #include "../../pipes.h"
 
 void externalHandler(struct DisplayPipes);
+void openDatabase(sqlite3**);
 
 #endif
