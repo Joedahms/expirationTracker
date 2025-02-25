d0ffc694c562ee82b94f941af6caec302fdbd357
lpelkey23 <lpelkey23@yahoo.com>
2025-02-25 02:38:05 +0000

    create model class interace

---
+++ vision/include/IModel.h
@@ -0,0 +1,31 @@
+#ifndef IMODEL_H
+#define IMODEL_H
+
+#include <filesystem>
+#include <string>
+#include <variant>
+
+class IModelHandler {
+public:
+  virtual ~IModelHandler() = default;
+
+  /**
+   * Runs the model script.
+   * @param imagePath Path to the image.
+   * @return Either a pair<int, float> (classification index & probability) or a string
+   * (extracted text).
+   */
+  virtual std::variant<std::pair<int, float>, std::string> runModel(
+      const std::filesystem::path& imagePath) const = 0;
+
+  /**
+   * Handles classification based on the model's return.
+   * @param imagePath Path to the image.
+   * @param foodItem Struct to update.
+   * @return Whether classification was successful.
+   */
+  virtual bool handleClassification(const std::filesystem::path& imagePath,
+                                    struct FoodItem& foodItem) const = 0;
+};
+
+#endif // IMODELHANDLER_H
