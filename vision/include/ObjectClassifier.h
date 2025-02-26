1cc5fe535a6297709f7df81a6074db0ec58627d1
lpelkey23 <lpelkey23@yahoo.com>
2025-02-26 00:32:26 +0000

    updated objectclassifier.h

--- vision/include/ObjectClassifier.h
+++ vision/include/ObjectClassifier.h
@@ -2,13 +2,9 @@
 #define OBJECT_CLASSIFICATION_H
 
 #include "../../food_item.h"
-#include "../include/helperFunctions.h"
 #include "IModel.h"
-#include "validateDetection.h"
 #include <filesystem>
 #include <glog/logging.h>
-#include <iostream>
-#include <variant>
 
 class ObjectClassifier : public IModel {
 public:
