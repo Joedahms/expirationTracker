831a906b9e68601f5e0c9fd47981d33e06737a2d
lpelkey23 <lpelkey23@yahoo.com>
2025-03-06 14:25:00 +0000

    added python server and text classifier endpoints

--- endpoints.h
+++ endpoints.h
@@ -4,9 +4,11 @@
 #include <string>
 
 struct ExternalEndpoints {
-  const std::string visionEndpoint   = "ipc:///tmp/vision_endpoint";
-  const std::string hardwareEndpoint = "ipc:///tmp/hardware_endpoint";
-  const std::string displayEndpoint  = "ipc:///tmp/display_endpoint";
+  const std::string visionEndpoint         = "ipc:///tmp/vision_endpoint";
+  const std::string hardwareEndpoint       = "ipc:///tmp/hardware_endpoint";
+  const std::string displayEndpoint        = "ipc:///tmp/display_endpoint";
+  const std::string textClassifierEndpoint = "ipc:///tmp/text_classifier_endpoint";
+  const std::string pythonServerEndpoint   = "ipc:///tmp/python_server_endpoint";
 };
 
 #endif
