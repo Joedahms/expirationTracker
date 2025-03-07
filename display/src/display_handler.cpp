fa93e1581e8318d636b24f6bdedd35b08d378f71
Joedahms <dahms.joejoe@gmail.com>
2025-03-06 19:53:15 -0500

    display crash

--- display/src/display_handler.cpp
+++ display/src/display_handler.cpp
@@ -15,7 +15,7 @@ DisplayHandler::DisplayHandler(zmqpp::context& context, const std::string& engin
       requestVisionSocket(context, zmqpp::socket_type::request),
       replySocket(context, zmqpp::socket_type::reply), logger("display_handler.txt") {
   try {
-    this->requestEngineSocket.connect(this->engineEndpoint);
+    this->requestEngineSocket.connect(engineEndpoint);
     this->requestHardwareSocket.connect(ExternalEndpoints::hardwareEndpoint);
     this->requestVisionSocket.connect(ExternalEndpoints::visionEndpoint);
     this->replySocket.bind(ExternalEndpoints::displayEndpoint);
@@ -51,42 +51,39 @@ void DisplayHandler::handle() {
         LOG(FATAL) << e.what();
       }
 
-      try {
-        this->logger.log("Waiting for message from vision");
-        std::string request;
-        this->replySocket.receive(request);
-        if (request == Messages::ITEM_DETECTION_FAILED) {
-          this->logger.log("Item detection failed");
-          // TODO Display failure message
-        }
-        else if (request == Messages::ITEM_DETECTION_SUCCEEDED) {
-          this->replySocket.send(Messages::AFFIRMATIVE);
-          this->logger.log("Item detection succeeded, receiving food item");
-          FoodItem foodItem;
-          receiveFoodItem(this->replySocket, Messages::AFFIRMATIVE, foodItem);
+      this->logger.log("Waiting for message from vision");
+      std::string request;
+      this->replySocket.receive(request);
+      if (request == Messages::ITEM_DETECTION_FAILED) {
+        this->replySocket.send(Messages::AFFIRMATIVE);
+        this->logger.log("Item detection failed");
+        // TODO Display failure message
+      }
+      else if (request == Messages::ITEM_DETECTION_SUCCEEDED) {
+        this->replySocket.send(Messages::AFFIRMATIVE);
+        this->logger.log("Item detection succeeded, receiving food item");
+        FoodItem foodItem;
+        receiveFoodItem(this->replySocket, Messages::AFFIRMATIVE, foodItem);
 
-          this->logger.log("Food item received, storing in database");
-          sqlite3* database = nullptr;
-          openDatabase(&database);
-          storeFoodItem(database, foodItem);
-          sqlite3_close(database);
-          this->logger.log("Food item stored in database");
+        this->logger.log("Food item received, storing in database");
+        sqlite3* database = nullptr;
+        openDatabase(&database);
+        storeFoodItem(database, foodItem);
+        sqlite3_close(database);
+        this->logger.log("Food item stored in database");
 
-          this->requestEngineSocket.send(Messages::ITEM_DETECTION_SUCCEEDED);
-          std::string engineResponse;
-          this->requestEngineSocket.receive(engineResponse);
-          if (engineResponse == Messages::AFFIRMATIVE) {
-            this->logger.log("Item stored in database and engine notified of success");
-          }
-          else {
-            LOG(FATAL) << "Unexpected message received";
-          }
+        this->requestEngineSocket.send(Messages::ITEM_DETECTION_SUCCEEDED);
+        std::string engineResponse;
+        this->requestEngineSocket.receive(engineResponse);
+        if (engineResponse == Messages::AFFIRMATIVE) {
+          this->logger.log("Item stored in database and engine notified of success");
         }
         else {
           LOG(FATAL) << "Unexpected message received";
         }
-      } catch (const zmqpp::exception& e) {
-        LOG(FATAL) << e.what();
+      }
+      else {
+        LOG(FATAL) << "Unexpected message received";
       }
     }
   }
