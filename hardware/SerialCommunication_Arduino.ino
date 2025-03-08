80cfa538fd8666eb06229d3a59104e4731c616e0
geromyc <geromycunningham@gmail.com>
2025-03-08 14:42:10 -0500

    clang works on Arduino code lol

--- hardware/SerialCommunication_Arduino.ino
+++ hardware/SerialCommunication_Arduino.ino
@@ -1,6 +1,7 @@
 
 /*Serial communication from Arduino to Raspberry Pi
-Currently a number x is sent from the Raspberry Pi to the Aruduino. The Arduino acts accordingly to which number is sent, then sends a "confirmation" back to the Pi. 
+Currently a number x is sent from the Raspberry Pi to the Aruduino. The Arduino acts
+accordingly to which number is sent, then sends a "confirmation" back to the Pi.
 */
 
 int x;
@@ -13,16 +14,17 @@ void setup() {
 }
 
 void loop() {
-    while (!Serial.available());
-    x = Serial.readString().toInt(); //Reading from the serial port
-    
-    //determining what action to take based on what was sent from Pi
-    if (x == 4) {
-      digitalWrite(LED, HIGH);
-      Serial.println("LED is on");
-    }
-    else if (x == 8) {
-      digitalWrite(LED, LOW);
-      Serial.println("LED is off");
-    }
+  while (!Serial.available())
+    ;
+  x = Serial.readString().toInt(); // Reading from the serial port
+
+  // determining what action to take based on what was sent from Pi
+  if (x == 4) {
+    digitalWrite(LED, HIGH);
+    Serial.println("LED is on");
+  }
+  else if (x == 8) {
+    digitalWrite(LED, LOW);
+    Serial.println("LED is off");
+  }
 }
