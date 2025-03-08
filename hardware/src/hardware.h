adbd70ddfbc24dbd846d6a5edbb49a3a09f7e1c6
lpelkey23 <lpelkey23@yahoo.com>
2025-03-08 05:26:23 +0000

    currently hardcoded for testing. will fix later

--- hardware/src/hardware.h
+++ hardware/src/hardware.h
@@ -27,7 +27,7 @@ private:
   zmqpp::socket requestDisplaySocket;
   zmqpp::socket replySocket;
 
-  const std::string IMAGE_DIRECTORY = "/home/pi/Documents/raspi-yolo/images/temp/";
+  const std::string IMAGE_DIRECTORY = "/home/pi5/Documents/raspi-yolo/images/temp/";
 
   // // Code for Geromy
   // const std::string IMAGE_DIRECTORY =
