33bf3be091a66c61189aecebe372fbe4b61f1dba
Joedahms <dahms.joejoe@gmail.com>
2025-04-04 13:05:31 -0400

    unique to shared

--- display/src/engine/elements/panel.cpp
+++ display/src/engine/elements/panel.cpp
@@ -61,7 +61,7 @@ void Panel::addFoodItem(const FoodItem& foodItem, const SDL_Point& relativePosit
   addFoodItemName(foodItem, relativePosition);
   addFoodItemExpirationDate(foodItem, relativePosition);
 
-  std::unique_ptr<NumberSetting> itemQuantity = std::make_unique<NumberSetting>(
+  std::shared_ptr<NumberSetting> itemQuantity = std::make_shared<NumberSetting>(
       this->displayGlobal, SDL_Rect{0, 0, 0, 0}, this->id, this->logFile);
   addElement(std::move(itemQuantity));
 }
