82e6acb96824fa7443f20d17ded49757f7cd47b2
Joedahms <dahms.joejoe@gmail.com>
2025-05-05 19:41:32 -0400

    enter

--- display/src/display_engine.cpp
+++ display/src/display_engine.cpp
@@ -162,7 +162,7 @@ void DisplayEngine::handleStateChange() {
     switch (currentState) {
     case EngineState::SCANNING:
       this->engineState = this->scanning.get();
-      this->scanning.enter();
+      this->scanning->enter();
       break;
 
     case EngineState::ITEM_LIST:
